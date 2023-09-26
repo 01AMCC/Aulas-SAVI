1c4fee0414aff4c2762f0a00a5eb05e828cc72b1
01AMCC <amcc@ua.pt>
2023-09-26 13:45:41 +0100

    update ex3

--- Aula2/Ex3/School.py
+++ Aula2/Ex3/School.py
@@ -3,19 +3,44 @@ import copy
 import cv2
 import numpy
 
+point_start = None
+point_end= None
+
+def drawfunction(event,x,y,flags,param):
+    global point_start, point_end, template
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        point_start = (x,y)
+        print(point_start)
+        
+    elif event == cv2.EVENT_LBUTTONUP :
+        point_end = (x,y)
+        print(point_end)
+
+        template = scene [point_start[1] : point_end[1], point_start[0] : point_end[0]]
+        cv2.imshow('template', template)
+
+
+
 def main():
 
     scene = cv2.imread('../Images/school.jpg')
-    template = cv2.imread('../Images/wally.png')
 
-    result = cv2.matchTemplate(scene, template, cv2.TM_CCOEFF_NORMED)
+    #template = scene [point_start[1] : point_end[1], point_start[0] : point_end[0]]
+    #cv2.imshow('template', template)
+   
+    cv2.namedWindow('scene')
+    cv2.setMouseCallback('scene', drawfunction)
     
-    _, value_max, _, max_loc = cv2.minMaxLoc(result)
-    print(value_max)
-    print(max_loc)
 
-    h , w , _= template.shape
-    cv2.rectangle(scene, (max_loc[0], max_loc[1]),(max_loc[0]+w, max_loc[1]+h), (0 , 255 , 0),4)
+    #result = cv2.matchTemplate(scene, template, cv2.TM_CCOEFF_NORMED)
+    
+    #_, value_max, _, max_loc = cv2.minMaxLoc(result)
+    #print(value_max)
+    #print(max_loc)
+
+    #h , w , _= template.shape
+    #cv2.rectangle(scene, (max_loc[0], max_loc[1]),(max_loc[0]+w, max_loc[1]+h), (0 , 255 , 0),4)
     
     cv2.imshow("Scene", scene)
     cv2.waitKey(0)
